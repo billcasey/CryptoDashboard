3a5801bda775aa379924f55ee114ff431d82eaa1
Bill <billc@smu.edu>
2017-12-08 21:20:10 -0600

    Initializing the project as a single view Swift App.

---
+++ CryptoDashboard/CryptoDashboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoDashboard
+//
+//  Created by Bill on 12/8/17.
+//  Copyright © 2017 Bill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
